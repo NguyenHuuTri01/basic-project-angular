5752a459bf5d348d6bef15fcb75e218ebd9c60e8
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2023-09-21 10:11:25 +0700

    display, delete product

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data-type';
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
-export class SellerHomeComponent {
+export class SellerHomeComponent implements OnInit {
+  productList: Product[] | undefined;
+  productMessage: undefined | string;
 
+  constructor(private product: ProductService) { }
+
+  ngOnInit(): void {
+    this.getListProduct();
+  }
+
+  deleteProduct(id: number) {
+    const response = confirm("Are you sure you want to delete this item?");
+
+    if (response) {
+      this.product.deleteProduct(id).subscribe((result) => {
+        if (result) {
+          this.productMessage = 'Product is deleted';
+          this.getListProduct();
+        }
+      })
+      setTimeout(() => {
+        this.productMessage = undefined;
+      }, 3000)
+    } else { }
+  }
+  getListProduct() {
+    this.product.productList().subscribe((result) => {
+      if (result) {
+        this.productList = result;
+      }
+    })
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,4 +12,10 @@ export class ProductService {
   addProduct(data: Product) {
     return this.http.post('http://localhost:3000/products', data);
   }
+  productList() {
+    return this.http.get<Product[]>('http://localhost:3000/products');
+  }
+  deleteProduct(id: number) {
+    return this.http.delete(`http://localhost:3000/products/${id}`)
+  }
 }
